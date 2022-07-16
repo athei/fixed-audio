b3917452b5647d0a99ead0ea3425db16140f3250
Alexander Theißen <alex.theissen@me.com>
2022-07-16 15:41:33 +0200

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use coreaudio::audio_unit::macos_helpers as helpers;
 mod sound;
 
+use coreaudio::audio_unit::macos_helpers as helpers;
+use crate::sound::set_default_device_id;
+
 fn main() {
-    let default = (
-        helpers::get_default_device_id(false),
-        helpers::get_default_device_id(true),
-    );
-    println!("default: {:?}", default);
+    let default = helpers::get_default_device_id(true).unwrap();
+    println!("default_input: {:?}", default);
     for id in helpers::get_audio_device_ids().unwrap() {
         let name = helpers::get_device_name(id).unwrap();
         println!("{}: {}", id, name);
     }
-    let _listener = sound::DefaultInputListener::new(Box::new(|device| {
+    let _listener = sound::DefaultInputListener::new(Box::new(move |device| {
         println!("callback: {}", device);
+        if device != default {
+            set_default_device_id(true, default);
+        }
     }));
     unsafe {
         coreaudio::sys::CFRunLoopRun();
