7c40605c79d51617fb1bf476e55c9666c8d875e6
Alexander Theißen <alex.theissen@me.com>
2022-07-16 15:55:23 +0200

    Fix output

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ use crate::sound::set_default_device_id;
 use coreaudio::audio_unit::macos_helpers as helpers;
 
 fn main() {
-    let default = helpers::get_default_device_id(true).unwrap();
-    println!("default_input: {:?}", default);
     for id in helpers::get_audio_device_ids().unwrap() {
         let name = helpers::get_device_name(id).unwrap();
         println!("{}: {}", id, name);
     }
+    let default = helpers::get_default_device_id(true).unwrap();
+    println!("Fixing input to device: {}", default);
     let _listener = sound::DefaultInputListener::new(move |device| {
-        println!("callback: {}", device);
         if device != default {
             set_default_device_id(true, default);
         }
